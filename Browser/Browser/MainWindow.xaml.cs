4a0eaec52a75ec4b9b4c573d9471d6f405813e8a
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2019-02-24 20:52:10 +0600

    Add files via upload

---
+++ Browser/Browser/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Browser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            wbSample.Navigate("http://www.mail.ru");
+        }
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                wbSample.Navigate(txtUrl.Text);
+        }
+
+        private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            txtUrl.Text = e.Uri.OriginalString;
+        }
+
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoForward();
+        }
+
+        private void GoToPage_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void GoToPage_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.Navigate(txtUrl.Text);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ComboBox comboBox = (ComboBox)sender;
+            //ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            ////MessageBox.Show(selectedItem.Content.ToString());
+            string text = (sender as ComboBox).SelectedItem as string;
+            wbSample.Navigate(text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> favourites = new List<string>();
+            favourites.Add(txtUrl.Text);
+           
+            Combo.ItemsSource = favourites;
+           
+        }
+    }
+}
